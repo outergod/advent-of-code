6fa4f2072b15e26d0bc7e4dfd27ba362a3cdca91
Alexander Dorn <ad@not.one>
2022-12-07 21:53:02 +0100

    Refactor day 4

--- src/outergod/day_4.clj
+++ src/outergod/day_4.clj
@@ -5,22 +5,18 @@
 
 (def input (slurp (io/resource "day-4")))
 
-(defn solve-1 [input]
+(defn solve [input rangefn]
   (count
    (filter identity
            (map (fn [line]
                   (let [[a1 b1 a2 b2] (->> line (re-matches #"(\d+)-(\d+),(\d+)-(\d+)") (drop 1) (map #(Integer/parseInt %)))
                         range1 (set (range a1 (inc b1)))
                         range2 (set (range a2 (inc b2)))]
-                    (or (superset? range1 range2) (subset? range1 range2))))
+                    (rangefn range1 range2)))
                 (s/split-lines input)))))
 
+(defn solve-1 [input]
+  (solve input #(or (superset? %1 %2) (subset? %1 %2))))
+
 (defn solve-2 [input]
-  (count
-   (filter identity
-           (map (fn [line]
-                  (let [[a1 b1 a2 b2] (->> line (re-matches #"(\d+)-(\d+),(\d+)-(\d+)") (drop 1) (map #(Integer/parseInt %)))
-                        range1 (set (range a1 (inc b1)))
-                        range2 (set (range a2 (inc b2)))]
-                    (not (empty? (intersection range1 range2)))))
-                (s/split-lines input)))))
+  (solve input #(not (empty? (intersection %1 %2)))))
